a37cb4ee0c4d6f29587dc5e41472c5b2cc1d4a2b
Benjamin Lowry <lowryb1@rchk.edu.hk>
2017-12-17 17:32:42 +0800

    First draft of Usable and Weapon classes

---
+++ Usables/Weapon.py
@@ -0,0 +1,30 @@
+from Usables.Usable import Usable
+from Entities.Entity import Entity
+
+class Weapon(Usable):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.damage = 0
+        self.attackSpeed = 0
+
+        self.ammunition = 0  # -1 for melee weapons
+        self.meleeWeapon = True
+
+        self.durability = 1  # From 0 to 1; the inverse of the probability of it breaking after one use
+        self.durabilityDecayConstant = 0.01  # How much the durability constant decreases with each use
+
+
+    def use(self, object):
+
+        super().use(object)
+        print("It is a weapon.")
+
+
+weapon = Weapon()
+
+entity = Entity()
+
+weapon.use(entity)
