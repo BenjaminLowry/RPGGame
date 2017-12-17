c8577ee536b437e57e837ed6414982cb667721f7
JaerJaer <lowryj1@rchk.edu.hk>
2017-12-17 16:54:20 +0800

    Added different aspects of the entities, added human class

--- Entities/Entity.py
+++ Entities/Entity.py
@@ -1,4 +1,17 @@
 
 class Entity(object):
-    health = 129312
-    
+    def __init__(self, *args, **kwargs):
+        health = 0
+        naturalDamage = 0
+        speed = 0
+        strength = 0
+        intelligence = 0
+
+
+
+
+
+print ("|   | ____ |   ____\n"
+       "|___| |    |   |   |\n"
+       "|   | |--  |   |   |\n"
+       "|   | |___ |___|___|")
---
+++ Entities/Human.py
@@ -0,0 +1,3 @@
+class Human(Entity):
+    def __init__(self, *args, **kwargs):
+        super.health = 100
