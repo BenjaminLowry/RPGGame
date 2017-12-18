ad340148ad5215d746b40df06730898532168f08
Benjamin Lowry <lowryb1@rchk.edu.hk>
2017-12-18 13:59:05 +0800

    Restructuring and creation of Scenes

---
+++ Scenes/Scene.py
@@ -0,0 +1,11 @@
+
+class Scene:
+
+    def __init__(self):
+        self.location = ""
+        self.openingDescription = ""
+        self.objects = []
+        self.exits = []  # [Object: Scene]
+        self.usables = []
+        # self.consumables = []
+        # self.
