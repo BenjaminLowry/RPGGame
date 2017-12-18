4ed8d0019e97fe6ae5ca134ac44777ebbb2e5174
Benjamin Lowry <lowryb1@rchk.edu.hk>
2017-12-18 14:51:04 +0800

    First scene is being created.

---
+++ Locations/Location.py
@@ -0,0 +1,12 @@
+
+class Location:
+
+    def __init__(self, name, description, footprint, coordinates):
+
+        self.name = name
+        self.description = description
+        self.footprint = footprint  # String of bird's eye view of location
+        self.coordinates = coordinates  # (Int, Int)
+
+
+
--- Scenes/OpeningScene.py
+++ Scenes/OpeningScene.py
@@ -1,7 +1,22 @@
 from Scenes.Scene import Scene
+from Locations.Location import Location
+from Objects.Object import Object
 
 class OpeningScene(Scene):
 
     def __init__(self):
 
-        super().__init__()
+        location = Location("Bedroom", "You woke up here. You don't know why.", "------"
+                            "|    |"
+                            "------", (0, 0))
+
+        openingDescription = open("OpeningScene.txt", "r").read()
+
+        window = Object("A dirty window half-covered with old drapes. Can't seem much outside. It is daytime though.",
+                        None)
+        bed = Object("A metal bed frame with old mattress on it. Some mysterious stains present.")
+
+        super()._
+
+
+
