aec2e97cec81ca51e0571e03d6d853a3689fa1d0
JaerJaer <lowryj1@rchk.edu.hk>
2017-12-20 16:02:46 +0800

    Wrote the main GUI parts, added in a timer that switches each bit of text down the console every 5 seconds

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,12 +1,20 @@
 import tkinter as tk
+from Scenes.OpeningScene import OpeningScene
+import time
 
-class gui():
+
+
+class Gui:
+
     root = tk.Tk()
     def __init__(self, *args, **kwargs):
         # Window
         self.root.title("RPG Game")
         self.root.geometry("1000x1000")
+        self.openingDescription = OpeningScene
 
+
+    #frames
     invent = tk.Frame(root)
     invent.config(bd=10, relief="groove", bg="white", height=990, width=200)
     invent.grid(row=0, column=2, sticky="ne")
@@ -30,6 +38,31 @@ class gui():
     miscell = tk.Frame(invent)
     miscell.config(bd=3, relief="solid", bg="gray", height=200, width=180)
     miscell.grid(row=5, column=0)
+
+    #Frames for the scene description
+    firstSection = tk.Frame(root)
+    firstSection.config(bd=3, relief="ridge", height=200, width=350)
+    firstSection.grid(row=1, column=0)
+
+    secSection = tk.Frame(root)
+    secSection.config(bd=3, relief="ridge", height=200, width=350)
+    secSection.grid(row=2, column=0)
+
+    triSection = tk.Frame(root)
+    triSection.config(bd=3, relief="ridge", height=200, width=350)
+    triSection.grid(row=3, column=0)
+
+    fourSection = tk.Frame(root)
+    fourSection.config(bd=3, relief="ridge", height=200, width=350)
+    fourSection.grid(row=4, column=0)
+
+    fivSection = tk.Frame(root)
+    fivSection.config(bd=3, relief="ridge", height=200, width=350)
+    fivSection.grid(row=5, column=0)
+
+    sexSection = tk.Frame(root)
+    sexSection.config(bd=3, relief="ridge", height=200, width=350)
+    sexSection.grid(row=6, column=0)
 
     # Parts of the inventory
     inventory = tk.Label(invent, text="Inventory:     "
@@ -64,7 +97,7 @@ class gui():
                       text="                                                                                        ")
 
     # Scene text
-    sceneText = tk.Label(root, text="This is what happens in the scene.")
+    sceneText = tk.Label(root, text="                  ")
     sceneText.config(font=("Courier", 20, "bold", "underline"))
 
     # Packings
@@ -77,7 +110,80 @@ class gui():
     tools.pack()
     wearables.pack()
 
+
+    t=5
+
+    firstText = tk.Label(text=descript[0])
+    secText = tk.Label(text=descript[1])
+    triText = tk.Label(text=descript[2])
+    fourText = tk.Label(text=descript[3])
+    fivText = tk.Label(text=descript[4])
+    sexText = tk.Label(text=descript[5])
+
+    while t >= 0:
+        time.sleep(1)
+        t -= 1
+    firstText.config(firstSection)
+    firstText.pack()
+
+    t = 5
+    while t >= 0:
+        time.sleep(1)
+        t -= 1
+    firstText.config(secSection)
+    firstText.pack()
+
+    secText.config(firstSection)
+    secText.pack()
+
+    while t >= 0:
+        time.sleep(1)
+        t -= 1
+    firstText.config(triSection)
+    firstText.pack()
+
+    secText.config(secSection)
+    secText.pack()
+
+    triText.config(firstSection)
+    triText.pack()
+
+    while t >= 0:
+        time.sleep(1)
+        t -= 1
+    firstText.config(fourSection)
+    firstText.pack()
+
+    secText.config(triSection)
+    secText.pack()
+
+    triText.config(secSection)
+    triText.pack()
+
+    fourText.config(firstSection)
+    fourText.pack()
+
+    while t >= 0:
+        time.sleep(1)
+        t -= 1
+    firstText.config(fivSection)
+    firstText.pack()
+
+    secText.config(fourSection)
+    secText.pack()
+
+    triText.config(triSection)
+    triText.pack()
+
+    fourText.config(secSection)
+    fourText.pack()
+
     # Frames
     root.mainloop()
+
+
+
+
+
 
 
--- Scenes/OpeningScene.py
+++ Scenes/OpeningScene.py
@@ -6,15 +6,18 @@ class OpeningScene(Scene):
 
     def __init__(self):
 
-        location = Location("Bedroom", "You woke up here. You don't know why.", "------"
+        self.location = Location("Bedroom", "You woke up here. You don't know why.", "------"
                             "|    |"
                             "------", (0, 0))
 
-        openingDescription = open("OpeningScene.txt", "r").read()
+        self.openingDescription = open("OpeningScene.txt", "r").read()
 
-        window = Object("A dirty window half-covered with old drapes. Can't seem much outside. It is daytime though.",
+        self.window = Object("A dirty window half-covered with old drapes. Can't seem much outside. It is daytime though.",
                         None)
-        bed = Object("A metal bed frame with old mattress on it. Some mysterious stains present.")
+        self.bed = Object("A metal bed frame with old mattress on it. Some mysterious stains present.")
+
+        self.openingDescript = ["You wake up in a dimly lit room. You're laying on a bed.", "Hmm... there aren't sheets on the bed.", 'Small strands of sunlight illuminate the wall to your right.', 'There’s a socket on the ceiling where a lightbulb used to be. To the left of the bed there’s a dresser. One of its handles is broken.', 'There’s a white wooden door on the other side of the room. It’s shut.', 'You hear a growling noise off in the distance.']
+
 
         super()._
 
