edcdf18c714e25e5608ea275ada71db9ab1e737e
Benjamin Lowry <lowryb1@rchk.edu.hk>
2018-01-03 13:49:53 +0800

    Scene parsing drafted

--- Scenes/Scene.py
+++ Scenes/Scene.py
@@ -13,8 +13,14 @@ class Scene:
 
     def get_opening_description(self):
         return self.openingDescription.split(". ")
- 
+
     def parse(self, commandText):
+
+        errorMessage = "Invalid request. Please try again."
+
+        # If the user does not put a colon, throw error
+        if not str(commandText).__contains__(":"):
+            return errorMessage
 
         (command, objectName) = str(commandText).lower().split(":")
 
@@ -26,10 +32,16 @@ class Scene:
                 selectedObject = object
                 break
 
+        # If the user inputs an invalid object, throw error
+        if selectedObject is None:
+            return errorMessage
+
         if command == "go to":
             return self.go_to(selectedObject)
         elif command == "look under":
             pass
+        else:  # If the command is invalid, throw error
+            return errorMessage
 
     def go_to(self, object):
 
